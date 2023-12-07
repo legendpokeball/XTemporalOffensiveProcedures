c4bc1290d9b40b1355c779758ec236651f7ea90a
legendpokeball <149884570+legendpokeball@users.noreply.github.com>
2023-12-07 11:10:27 +0000

    seleniumbase

--- spin.py
+++ spin.py
@@ -1,116 +1,64 @@
-import json
-import subprocess
-import fileinput
-import time
-import random
-import undetected_chromedriver as uc
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from fake_useragent import UserAgent
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from datetime import datetime, timedelta
-
-from workaround import update_executable_path
 import os
+try:
+  from selenium_stealth import stealth
+except ImportError:
+  os.system("pip install selenium_stealth")
+  from selenium_stealth import stealth
 
-ua = UserAgent().random
-chromedriver_path = update_executable_path()
+try:
+  from seleniumbase import SB
+except ImportError:
+  os.system("pip install seleniumbase")
+  from seleniumbase import SB
 
-executable_path = chromedriver_path
-os.system(f"chmod 700 {executable_path}")
+from seleniumbase import SB
+from selenium_stealth import stealth
+import random
+
+
+def apply_stealth_to_tab(driver):
+  stealth(
+      driver,
+      languages=["en-US", "en"],
+      vendor="Google Inc.",
+      platform="Win32",
+      webgl_vendor="Intel Inc.",
+      renderer="Intel Iris OpenGL Engine",
+      fix_hairline=True,
+  )
+  # stealth(
+  #     sb.driver,
+  #     user_agent=
+  #     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
+  #     languages=["en-US", "en"],
+  #     vendor="Google Inc.",
+  #     platform="Win32",
+  #     webgl_vendor="Intel Inc.",
+  #     renderer="Intel Iris OpenGL Engine",
+  #     fix_hairline=True,
+  #     run_on_insecure_origins=True,
+  # )
+  tz_params = {'timezoneId': 'America/New_York'}
+  driver.execute_cdp_cmd('Emulation.setTimezoneOverride', tz_params)
 
 
 def perform(url, method):
+  with SB(uc=True, headed=True) as sb:
+    apply_stealth_to_tab(sb.driver)
+    sb.driver.get("https://bot.sannysoft.com/")
+    sb.sleep(random.randint(4, 7))
 
-  options = uc.ChromeOptions()
-  # options.add_argument(f'--user-agent={ua}')
-  options.add_argument("--no-sandbox")
-  options.add_argument('--profile-directory=Default')
-  options.add_argument('--disable-plugins-discovery')
-  options.add_argument('--start-maximized')
-  options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--disable-gpu")
-  options.add_argument("--disable-extensions")
-  options.add_argument("--disable-popup-blocking")
-  options.add_argument("--disable-infobars")
-  options.add_argument("--disable-notifications")
-  # options.add_argument("--headless=False")
-  # options.add_argument("--disable-web-security")
-  # options.add_argument("--disable-features=VizDisplayCompositor")
-  options.add_argument("--disable-features=NetworkService")
-  # options.add_argument("--disable-features=VizHitTestSurfaceLayer")
-  # options.add_argument("--disable-features=VizHitTestDrawQuad")
-  # options.add_argument("--disable-features=VizHitTestDrawQuad")
-  options.add_argument("--enable-javascript")
-  options.add_argument("--disk-cache-size=0")
-  count = 0
-  exit_flag = False
-  # driver = uc.Chrome(options=options)
-  driver = uc.Chrome(options=options, driver_executable_path=executable_path)
-  # driver.get(url)
-  wait = WebDriverWait(driver, 20)
-  # # wait = WebDriverWait(driver, 10)
+    apply_stealth_to_tab(sb.driver)
+    sb.driver.get(url)
+    if method == 'ld':
+      sb.driver.uc_click('a')
 
-  # wait_time = random.uniform(5, 15)
-  # time.sleep(wait_time)
+    sb.sleep(random.randint(2, 4))
+    sb.click('button:contains("Download")')
+    print('Clicked download button')
+    print("waiting 50-60 secs")
+    sb.sleep(random.randint(40, 55))
+    sb.driver.quit()
+    return True
 
-  try:
-    print('1')
-    driver.get(url)
-    if method == 'ig':
-      print('1')
-      driver.find_element(
-          By.CSS_SELECTOR,
-          "body > div > div.page.-cx-PRIVATE-Page__body.-cx-PRIVATE-Page__body__ > div > div > p:nth-child(3) > button"
-      ).click()
-    elif method == 'ld':
-      print('l')
-      time.sleep(3)
-      driver.find_element(By.CSS_SELECTOR, ".main > a").click()
 
-    else:
-      print("no method speccified method")
-    time.sleep(random.uniform(5, 8))
-    original_window = driver.current_window_handle
-
-    # download_button = driver.find_element(By.ID, 'downloadbtn')
-    print('1')
-    element_locator = (By.ID, 'downloadbtn')
-    while True:
-      try:
-        print('enter function')
-
-        # Click anywhere on the page (body)
-        driver.find_element(By.TAG_NAME, 'body').click()
-        time.sleep(4)
-
-        # Check if it redirects
-        if len(driver.window_handles) > 1:
-          driver.switch_to.window(driver.window_handles[-1])
-          driver.close()
-          driver.switch_to.window(original_window)
-          print('Redirected, closing new window')
-          continue
-
-        # Try clicking the download button
-        # element = driver.find_element(*element_locator)
-        print('try')
-        element = wait.until(EC.element_to_be_clickable(element_locator))
-        element.click()
-
-        print('Clicked download button')
-        print("waiting 50-60 secs")
-        time.sleep(random.randint(60, 80))
-        driver.quit()
-        return True
-
-      except:
-        print('Retry')
-
-  except:
-    print(Exception)
-    return False
-  finally:
-
-    driver.quit()
